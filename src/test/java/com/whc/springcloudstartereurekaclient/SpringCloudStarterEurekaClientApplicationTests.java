5ee82429077682cdd0b542b0dbe87609b198afd9
whc <1532106001@qq.com>
2019-04-08 12:48:49 +0800

    first upload

---
+++ src/test/java/com/whc/springcloudstartereurekaclient/SpringCloudStarterEurekaClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.whc.springcloudstartereurekaclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudStarterEurekaClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
